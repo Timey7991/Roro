1fc23ded72473ecd676f6882935e094316383a0a
Arvie <arviedelgado@outlook.com>
2018-09-11 05:47:52 +0800

    Roro WPF - Added ValueOptionsAttribute class.

--- src/Roro.Activities/DefaultValueAttribute.cs
+++ src/Roro.Activities/DefaultValueAttribute.cs
@@ -5,9 +5,11 @@ namespace Roro.Activities
     [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Property, AllowMultiple = false)]
     public class DefaultValueAttribute : Attribute
     {
-        public DefaultValueAttribute(object defaultValue)
-        {
+        public object Value { get; }
 
+        public DefaultValueAttribute(object value)
+        {
+            this.Value = value;
         }
     }
 }
---
+++ src/Roro.Activities/ValueOptionsAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Roro.Activities
+{
+    [AttributeUsage(AttributeTargets.Interface | AttributeTargets.Property, AllowMultiple = false)]
+    public class ValueOptionsAttribute : Attribute
+    {
+        public IEnumerable<object> Values { get; }
+
+        public ValueOptionsAttribute(params object[] values)
+        {
+            this.Values = values;
+        }
+    }
+}
